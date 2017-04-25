06018dfcd47e51687c2158a463acdb34e32e0c7d
Herman-Scheer <g@herman-scheer.com>
2017-04-25 09:52:51 -0700

    feat: messages functionality

--- modules/client/main.ts
+++ modules/client/main.ts
@@ -26,26 +26,40 @@ export class Store {
   }
 }
 
-// actions
-const INCREMENT = 'INCREMENT'
-const DECREMENT = 'DECREMENT'
-const PLUS = 'PLUS'
+// ----------
 
-export const incrementAction = { type: INCREMENT }
-export const decrementAction = { type: DECREMENT }
-export const unknownAction = { type: 'UNKNOWN' }
-export const plusAction = { type: PLUS, payload: 7 }
+export const ADD_MESSAGE = 'ADD_MESSAGE'
+export const DELETE_MESSAGE = 'DELETE_MESSAGE'
 
-// reducers
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return state + 1
-    case DECREMENT:
-      return state - 1
-    case PLUS:
-      return state + action.payload
-    default:
-      return state
+export class MessageActions {
+  static addMessage (message) {
+    return { type: ADD_MESSAGE, message, created_at: new Date() }
+  }
+
+  static deleteMessage (index) {
+    return { type: DELETE_MESSAGE, index }
   }
 }
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_MESSAGE:
+      return { messages: state.messages.concat(action.message) }
+    case DELETE_MESSAGE:
+      let idx = action.index
+      return { messages: [
+        ...state.messages.slice(0, idx),
+        ...state.messages.slice(idx + 1, state.messages.length)
+      ]}
+    default:
+      return {}
+  }
+}
+
+let store = new Store(reducer, { messages: [] })
+
+store.dispatch(MessageActions.addMessage('Would you say the fringe was made of silk?'))
+store.dispatch(MessageActions.addMessage(`Wouldn't have no other kind of silk`))
+store.dispatch(MessageActions.addMessage('Has it really got a team of snow white horses'))
+
+console.log(store.getState())
