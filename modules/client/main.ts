989b94fe4716b599ad731ece01f4f6829ebc1511
Herman-Scheer <g@herman-scheer.com>
2017-04-25 16:28:31 -0700

    fix: refernce blog code

--- modules/client/main.ts
+++ modules/client/main.ts
@@ -2,6 +2,7 @@ import './polyfills'
 import './main.sass'
 
 // http://blog.ng-book.com/introduction-to-redux-with-typescript-and-angular-2/#core-redux-ideas
+// https://github.com/ng-book/angular2-redux-chat
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './components/app.client.module'
 
