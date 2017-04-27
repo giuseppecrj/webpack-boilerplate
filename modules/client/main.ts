bde9b9764b984cff766e588f2aa33b7429cd2768
Herman-Scheer <g@herman-scheer.com>
2017-04-26 19:05:36 -0700

    references

--- modules/client/main.ts
+++ modules/client/main.ts
@@ -3,6 +3,9 @@ import './main.sass'
 
 // http://blog.ng-book.com/introduction-to-redux-with-typescript-and-angular-2/#core-redux-ideas
 // https://github.com/ng-book/angular2-redux-chat
+// https://github.com/ngrx/store
+// https://egghead.io/courses/building-a-time-machine-with-angular-2-and-rxjs
+// https://gist.github.com/btroncone/a6e4347326749f938510
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './components/app.client.module'
 
